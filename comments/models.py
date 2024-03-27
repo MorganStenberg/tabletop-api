31652b3757e25000b84859cd3ec0446ca0209c2e
Morgan Stenberg <morganstenberg@gmail.com>
2024-03-27 14:22:48 +0000

    add models for comments and likes apps

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from reviews.models import Review
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Post
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
