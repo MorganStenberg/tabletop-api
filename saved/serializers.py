07a0d8124fce4baaafd09c9c6a0abf9d47f0869f
Morgan Stenberg <morganstenberg@gmail.com>
2024-03-28 14:08:10 +0000

    add validation to handle duplicate saves

--- saved/serializers.py
+++ saved/serializers.py
@@ -6,11 +6,19 @@ from .models import Save
 class SaveSerializer(serializers.ModelSerializer):
     """
     Serializer for the Save model.
+    Overriding create method, credit to Stackoverflow.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     review_title = serializers.ReadOnlyField(source='review.title')
     review_game = serializers.ReadOnlyField(source='review.game')
 
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'duplicate save not possible'
+            })
 
     class Meta:
         model = Save
