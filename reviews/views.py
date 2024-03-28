8c44aa73540bb4c5879cd17d1e256c178d5f63de
Morgan Stenberg <morganstenberg@gmail.com>
2024-03-28 12:50:05 +0000

    add review detail view and url

--- reviews/views.py
+++ reviews/views.py
@@ -3,11 +3,12 @@ from django.db.models import Count
 from rest_framework import generics, permissions
 from .models import Review
 from .serializers import ReviewSerializer
+from table_tt_api.permissions import IsOwnerOrReadOnly
 
 
 class ReviewList(generics.ListCreateAPIView):
     """
-    Lists reviews or create a review if logged in
+    Lists reviews or create a review if user is logged in
     """
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -19,3 +20,15 @@ class ReviewList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it the user is the owner.
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Review.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
