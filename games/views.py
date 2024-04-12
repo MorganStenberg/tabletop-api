426cebcee6efc7f7cae1b4bd66dc92d29973db47
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-12 06:59:19 +0000

    add missing import of Response

--- games/views.py
+++ games/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, filters
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from table_tt_api.permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
